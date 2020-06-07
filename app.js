4a1073beef00c4201d3dab39d793e450f2102c4f
ToobaNajeeb1 <64564204+ToobaNajeeb1@users.noreply.github.com>
2020-06-07 04:58:56 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,542 @@
+
+/*---------------------------------FIRST CHAPTER----------------------------------*/
+/*--------------------------------------TASK 1----------------------------------*/
+alert("Welcome to my First website")
+
+/*------------------------TASK 2--------------------*/
+var c=("JavaScript Alert");
+var a=3;
+var b=1;
+if(a>b){
+    alert("Error! please enter a valid password") 
+}
+/*--------------------------------- TASK 3-----------*/
+var a=3;
+var b=1;
+if(a>b){
+    alert("welcometo JS land.... \n Happy Coding!") 
+}
+
+/*----------------------- TASK 4--------------------*/
+     alert("welcometo JS land.............")
+     alert("Happy Coding") 
+
+/*---------------------------- TASK 5--------------------------*/
+
+var a=4;
+var b=2;
+if(a>b){
+    alert("Hello...I can run JS through my web browser's console") 
+}
+/*----------------------------------------CHAPTER-2nd----------------------------------*/
+/*-------------------------------------TASK 1--------------------------*/
+var x=2;
+var y=3;
+var z=x+y;
+document.write(z);
+
+/*-------------------------------------TASK 2--------------------------*/
+/*-------------------------------------TASK 3--------------------------*/
+var x=2;
+var y=3;
+if(y>x){
+    alert("Hello world")
+}
+
+/*-------------------------------------TASK 4--------------------------*/
+alert("Jhone Dhoe")
+alert("15 years old") 
+alert("Certified Application development") 
+
+
+
+/*-------------------------------------TASK 5--------------------------*/
+ alert("PIZZA\nPIZZ\nPIZ\nPI\nP")
+
+
+/*--------------------------------------------- TASK 6-----------------------------*/
+
+var name="MY Email Address is  example @example.com"
+alert(name);
+
+/*-----------------------------------------------TASK 7--------------------------*/
+
+var name="I am trying to learn from the book A smater way to learn Javascript"
+alert(name);
+
+/*--------------------------------------------------TASK 8-------------------------*/
+var name="yeah I can write a html content through javascript"
+document.write(name);
+
+
+/*---------------------------------------CHAPTER-3 ----------------------------------*/
+
+/*---------------------------------------- TASK 1  ----------------------------------*/
+var age="15 years old"
+alert(age);
+/*---------------------------------------- TASK 3 ----------------------------------*/
+ document.write("My Birthday Year is:" + "1999");
+/*---------------------------------------- TASK 4 ----------------------------------*/
+var x=prompt("Enter your Name")
+var x=prompt("Enter product Title")
+var x=prompt("Enter product Quantity")
+document.write("You name is:" + name +"<br>")
+document.write("Your physics marks is:" + physics + "<br>")
+
+/*----------------------------------------4 chapter---,--------------------------------*/
+/*-------------------------------------TASK 1------------------------------------------*/
+var x = 5;
+var y = 6;
+var z = 8;
+var a = x + y + z;
+document.write(a);
+/*-------------------------------------TASK 2------------------------------------------*/
+
+/*------------------------------------chapter 5--------------------------------------*/
+/*------------------------------------------------TASK 1------------------------------*/
+ a=3;
+ b=8;
+ c=a+b;
+ document.write("Sum of 3 and 5 is:8");
+ document.write("Subtraction of 5 to 3 is:2");
+ document.write("Multiplication of 3 and 5 is:15");
+ document.write("division of 3 and 5 is:0.6");
+
+ /*------------------------------------------------TASK 4------------------------------*/
+ /*----------------------------------------TASK 5-----------------------------------*/
+document.write("2" + "*" + "1" + "=" + 2*1 +"<br>")
+document.write("2" + "*" + "2" + "=" + 2*2 +"<br>")
+document.write("2" + "*" + "3" + "=" + 2*3 +"<br>")
+document.write("2" + "*" + "4" + "=" + 2*4 +"<br>")
+document.write("2" + "*" + "5" + "=" + 2*5 +"<br>")
+document.write("2" + "*" + "6" + "=" + 2*6 +"<br>")
+document.write("2" + "*" + "7" + "=" + 2*7 +"<br>")
+document.write("2" + "*" + "8" + "=" + 2*8 +"<br>")
+document.write("2" + "*" + "9" + "=" + 2*9 +"<br>")
+document.write("2" + "*" + "10" + "=" + 2*10 +"<br>")
+
+
+
+
+/*-----------------------------------------CHAPTER-6 TO 9-----------------------------------------*/
+
+/*----------------------------------------TASK  1-----------------------------------*/
+document.write("The value of a is 10" + "<br>");
+document.write("The value of ++a is 10" + "<br>");
+document.write("The value of a is 11" + "<br>");
+document.write("The value of a++ is 11" + "<br>");
+document.write("The value of a is 12" + "<br>");
+document.write("The value of --a is 11" + "<br>");
+document.write("The value of a is 10" + "<br>");
+/*----------------------------------------TASK  2-----------------------------------*/
+var a=2,b=1;
+var x=--a - --b + ++b + b--;
+var z=alert(x)
+
+/*----------------------------------------TASK  3-----------------------------------*/
+var x=prompt("Enter your Name")
+
+
+/*----------------------------------------TASK  4-----------------------------------*/
+document.write("5" + "*" + "1" + "=" + 5*1 +"<br>")
+document.write("5" + "*" + "2" + "=" + 5*2 +"<br>")
+document.write("5" + "*" + "3" + "=" + 5*3 +"<br>")
+document.write("5" + "*" + "4" + "=" + 5*4 +"<br>")
+document.write("5" + "*" + "5" + "=" + 5*5 +"<br>")
+document.write("5" + "*" + "6" + "=" + 5*6 +"<br>")
+document.write("5" + "*" + "7" + "=" + 5*7 +"<br>")
+document.write("5" + "*" + "8" + "=" + 5*8 +"<br>")
+document.write("5" + "*" + "9" + "=" + 5*9 +"<br>")
+document.write("5" + "*" + "10" + "=" + 5*10 +"<br>")
+
+
+
+
+
+
+/*--------------------------------------CHAPTER-9 to 11 ----------------------------------*/
+
+/*---------------------------------------input city karachi TASK 1-----------------------------------*/
+var x=prompt("Enter your city")
+if(x=="karachi")
+{
+    alert("Welcome to City of lights")
+}
+
+
+/*---------------------------------------Enter Your Gender TASK 2-----------------------------------*/
+   
+var x=prompt("Enter Your Gender")
+if(x=="male")
+{
+    alert("Good Morning Sir")
+}
+else if(x=="female")
+{
+    alert("Good Morning Ma’am")
+}
+/*--------------------------------------traffic light task  3-----------------------------------*/
+var x=prompt("Enter Signal Color")
+if(x=="red")
+{
+    alert("Must Stop")
+}
+ else if(x=="yellow")
+{
+    alert("Ready to move")
+}
+else if(x="green")
+{
+    alert("Move Now")
+}
+/*--------------------------------------fuel  task 4-----------------------------------*/
+
+var x=prompt("Enter fuel ")
+if(x==0.25)
+{
+    alert("Please refill the fuel in your car")
+}
+
+/*---------------------------------------MARK SHEET TASK 6-----------------------------------*/
+var math=prompt("Enter Your Maths Marks");
+var physics=prompt("Enter Your physics Marks");
+var english=prompt("Enter Your english Marks");
+var Obtained_marks=parseInt(math) + parseInt(physics) + parseInt(english) ;
+var percentage= Obtained_marks*100/300;
+document.write("Your math marks is:" + math + "<br>");
+document.write("Your physics marks is:" + physics + "<br>");
+document.write("Your english marks is:" + english + "<br>");
+document.write("Your percentage  is:" + percentage + "<br>");
+document.write("Your Remarks is:" + Remarks + "<br>");
+if(percentage>=80){
+    document.write("Your Grade is:A-1");
+}
+else if(percentage>=70){
+    document.write("Your Grade is:A");
+}
+if(percentage>=60){
+    document.write("Your Grade is:B");
+}
+if(percentage>=60){
+    document.write("Your Grade is:Fail");
+}
+
+
+if(percentage>=80){
+    document.write("Remarks:Excellent");
+}
+else if(percentage>=70){
+    document.write("Remarks:Good");
+}
+if(percentage>=60){
+    document.write("Remarks:You need to improve");
+}
+if(percentage>=60){
+    document.write("Remarks:Sorry");
+}
+/*--------------------------------------TASK 7----------------------------------*/
+var x=prompt("Enter secret number")
+if(x==7)
+{
+    alert(" “Bingo! Correct answer” ")
+}
+ else if(x==1)
+{
+    alert("“Close enough to the correct answer”.")
+}
+else if(x==2)
+{
+    alert("“Close enough to the correct answer”.")
+}
+else if(x==3)
+{
+    alert("“Close enough to the correct answer”.")
+}
+else if(x==4)
+{
+    alert("“Close enough to the correct answer”.")
+}
+else if(x==5)
+{
+    alert("“Close enough to the correct answer”.")
+}
+else if(x==6)
+{
+    alert("“Close enough to the correct answer”.")
+}
+else if(x==8)
+{
+    alert("“Close enough to the correct answer”.")
+}
+else if(x==9)
+{
+    alert("“Close enough to the correct answer”.")
+}
+else if(x==10)
+{
+    alert("“Close enough to the correct answer”.")
+}
+
+/*--------------------------------------TASK 8-----------------------------------*/
+ 
+var i =4;
+if (i % 3 == 0) {
+ alert("divisible by 3");
+} else {
+    alert("divisible not by 3");
+} 
+/*----------------------------------TASK 9------------------------------------------------*/
+var num = 2;
+document.write("Number = "+num+"<br>");
+if(num % 2 == 0) {
+   document.write('Number is even!');
+} else {
+   document.write('Number is odd!');
+}
+
+var num = 3;
+document.write("Number = "+num+"<br>");
+if(num % 2 == 0) {
+   document.write('Number is even!');
+} else {
+   document.write('Number is odd!');
+}
+
+
+/*--------------------------------------temperature 10-----------------------------------*/
+
+var x=prompt("Enter Temperature")
+if(x>=40)
+{
+    alert("It is too hot outside.")
+}
+ else if(x>=30)
+{
+    alert(" The Weather today is Normal.")
+}
+else if(x>=20)
+{
+    alert("Today’s Weather is cool.")
+}
+else if(x>=10)
+{
+    alert("OMG! Today’s weather is so Cool.")
+}
+
+
+
+/*------------------------------chapter-12 13----------------------------------------------------*/
+/*-------------------------------------------------tast 1------------------------------------------------*/
+var x=prompt("Enter a alphabat 0r number")
+if(x >= 'A' && x <= 'Z'){
+alert("Given alphabat is upper case")
+}
+else if(x >= 'a' && x <= 'z')
+{
+    alert("Given alphabat is lower case")
+}
+else if(x >=1)
+{
+    alert("It is a number")
+}
+/*-------------------------------------------------tast 2------------------------------------------------*/
+var x=prompt("Enter a number 1")
+
+var y=prompt("Enter a number 2")
+if(x>y){
+    alert("x is greater!")
+}
+ else if(x<y){
+    alert("y is greater!")
+}
+else if(x==y){
+    alert("Both are equal!")
+}
+
+
+/*-------------------------------------------------tast 3------------------------------------------------*/
+
+ var x=prompt("Enter a number")
+
+if(x>0)
+{
+    alert("Given number is positive")
+}
+if(x<0)
+{
+    alert("Given number is negative")
+}
+if(x=0)
+{
+    alert("Given number is zero")
+}
+/*-------------------------------------------------TASK 4-----------------------------------------------*/
+if (char === 'a'||'e'||'i'||'o'||'u') {
+    document.write("The character is a vowels")
+}
+else {
+return "The character is a Consonant";
+}
+/*-------------------------------------------------TASK 5-----------------------------------------------*/
+var x=prompt("Enter password")
+var y=prompt("Enter  again password")
+if(x==y){
+    alert("Correct!")
+}
+else if(x!==y){
+    alert("Incorrect!")
+}
+
+
+/*--------------------------------------TASK 6-----------------------------------------------------------*/
+
+var x=prompt("Enter hour")
+if(x<18)
+{
+    alert("Good Day.")
+}
+ else if(x>18)
+{
+    alert("Good Evening")
+}
+/*--------------------------------------TASK 7-----------------------------------------------------------*/
+var x=prompt("Enter a time")
+if(x<="12am"){
+    alert("Good Morning Dear!")
+}
+else if(x<="6pm"){
+    alert("Good Ajternoon Dear!")
+}
+else if(x<="12pm"){
+    alert("Good Evening Dear!")
+}
+else if(x<="6am"){
+    alert("Good Evening Dear!")
+}
+/*------------------------------------------ chapter 14 16 ----all--------------------------------------------------------*/
+/*-------------------------------------------------task1 --------------------------------------------------------------*/
+ arr1 = ["Tooba<br>","fiza<br>","kosar<br>"];
+ document.write(arr1);
+/*-------------------------------------------------task 3--------------------------------------------------------------*/
+ x=("Dog");
+alert("Dog")
+var y=("Cat");
+alert("Cat")
+// /*-------------------------------------------------TASK 4------------------------------------------------------------------------*/
+var cars = ["Saab", "Volvo", "BMW"];
+alert( cars)
+
+
+
+/*-------------------------------------------------TASK 5------------------------------------------------------------------------*/
+var cities=["karachi","lahore","islamabad","multan"];
+for(var i=0; i<cities.length;i++)
+    if(cities[i]==="karachi")
+    {
+        alert("Karachi is a cleanest cities!")
+       alert("True")
+    }
+
+
+
+/*-------------------------------------------------TASK 6------------------------------------------------------------------------*/
+
+var x = ["Saab", "12","Volvo", "@","BMW"];
+alert(x)
+
+/*-------------------------------------------------TASK 7------------------------------------------------------------------------*/
+var myArray = ["SSC","HSC","BCS","BS","MS","M. Phil." , "PhD"];
+for(i=0; i< myArray.length; i++){
+document.write(myArray[i]+"<br>");
+}
+/*-----------------------------------------------task 8--------------------------------------------------------------*/
+var a=prompt("Enter Your Name");
+ var b=prompt("Enter Your Marks");
+var c=prompt("Enter Your Name");
+ var d=prompt("Enter Your Marks");
+ var e=prompt("Enter Your Name");
+ var f=prompt("Enter Your Marks");
+ var Obtained_marks=parseInt(b)  ;
+ var percentage= Obtained_marks*100/500;
+ document.write("Score of :" +a + Obtained_marks+  "Percentage is:" + percentage );
+ document.write("Score of :" +c + Obtained_marks+  "Percentage is:" + percentage );
+ document.write("Score of :" +e + Obtained_marks+  "Percentage is:" + percentage );
+
+
+
+/*-------------------------------------------------TASK 15-----------------------------------------------------------------------*/
+var fruits = ["Apple", "Samsung", "Motorola", "Nokia","Sony&Haider"];
+document.write(fruits)
+/*---------------------------------------17 to 20 chapter---12456789--------------------------------*/
+
+arr= ["apple" + "<br>"  "banana" + "<br>","orange" +"<br>","strawberry"+"<br>"]
+document.write(arr);
+/*------------------------------------------------TASK 1---------------------------------------*/
+
+var Arr=[[1,2],\n[3,4],\n[5,6]];
+document.write(Arr);
+/*-------------------------------------------------TASK 3------------------------------------*/
+var x;
+for(x=1; x<=10;x++)
+document.write(x+"<br>")
+/*-------------------------------------------------TASK 5------------------------------------*/
+ arr =(“apple”, “banana”, “mango”, “orange”, “strawberry”);
+document.write(arr);
+var arr =["apple",\n "orange",\n"banana","mango","strawberry"]
+document.write(arr)
+ 
+/*-------------------------------------------------TASK  9------------------------------------*/
+numbers = [2, 4, 9, 2, 0, 16, 24]
+
+ 
+ var smallest = numbers[0];
+
+for (var i = 1; i < numbers.length; i++) {
+
+   if (numbers[i] < smallest) {
+    smallest = numbers[i];
+  }
+
+  
+  document.write(smallest);
+}
+
+/*-------------------------------------------------TASK  6------------------------------------*/
+for(int i=1; i<=10; i++)
+document.write(i);
+
+
+
+
+
+/*-------------------------------------------------TASK 8----------------------------------------------*/
+
+/*------------------------------------------ chapter 17 to 20 -----------------------------------------------------------*/
+ arr=["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16"]
+ document.write(arr);
+
+
+
+var x; for(x=1; x<=10;x++){
+    break;
+}
+document.write(x)
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
